2cf2f8e44ec59028bdb5c5ef95c30372383cc156
EnesBalaban <enesbalaban950@gmail.com>
2025-12-13 22:10:20 +0300

    Generating HTML for the plant pages on the website.

---
+++ FloraMind_V1/Views/Plant/Cicekli.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FloraMind_V1.Views.Plant
+{
+    public class CicekliModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FloraMind_V1/Views/Plant/Kaktus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FloraMind_V1.Views.Plant
+{
+    public class KaktuslerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
